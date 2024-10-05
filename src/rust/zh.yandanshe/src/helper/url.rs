6e3e2c70fb3261832ec295f283242f0f8d3b90b8
Aphronyx <aphronyx.su@gmail.com>
2024-10-05 17:30:25 +0800

    feat: get manga page result from filters page

---
+++ src/rust/zh.yandanshe/src/helper/url.rs
@@ -0,0 +1,199 @@
+extern crate alloc;
+
+use aidoku::{
+	error::{AidokuError, NodeError, Result},
+	helpers::uri::{encode_uri_component, QueryParameters},
+	prelude::format,
+	std::{html::Node, net::Request, String, Vec},
+	Filter, FilterType,
+};
+use alloc::string::ToString as _;
+use core::fmt::{Display, Formatter, Result as FmtResult};
+use strum::{Display, FromRepr, IntoStaticStr};
+
+#[expect(private_interfaces)]
+#[derive(Display)]
+#[strum(prefix = "https://yandanshe.com")]
+pub enum Url {
+	#[strum(to_string = "/{genre}{status}/page/{page}/?{query}")]
+	Filters {
+		genre: Genre,
+		status: Status,
+		page: i32,
+		query: FiltersQuery,
+	},
+}
+
+impl Url {
+	pub fn html(self) -> Result<Node> {
+		let html = Request::get(self.to_string()).html()?;
+
+		let is_blocked = html.select("title").text().read() == "您已被臨時封鎖";
+		if is_blocked {
+			return Err(AidokuError::from(NodeError::ParseError));
+		}
+
+		Ok(html)
+	}
+
+	const fn filters(
+		genre: Genre,
+		status: Status,
+		tags: Vec<String>,
+		mode: Mode,
+		sort_by: Sort,
+		page: i32,
+	) -> Self {
+		let query = FiltersQuery::new(tags, mode, sort_by);
+
+		Self::Filters {
+			genre,
+			status,
+			page,
+			query,
+		}
+	}
+}
+
+impl From<(Vec<Filter>, i32)> for Url {
+	fn from((filters, page): (Vec<Filter>, i32)) -> Self {
+		macro_rules! init {
+			($($filter:ident, $Filter:ident);+) => {
+				$(let mut $filter = $Filter::default();)+
+			};
+		}
+		init!(genre, Genre; status, Status; mode, Mode; sort_by, Sort);
+
+		let mut tags = Vec::new();
+
+		for filter in filters {
+			#[expect(clippy::wildcard_enum_match_arm)]
+			match filter.kind {
+				FilterType::Select => {
+					macro_rules! get_filter {
+						($Filter:ident) => {
+							filter
+								.value
+								.as_int()
+								.ok()
+								.and_then(|i| {
+									let index = i.try_into().ok()?;
+
+									$Filter::from_repr(index)
+								})
+								.unwrap_or_default()
+						};
+					}
+					match filter.name.as_str() {
+						"類型" => genre = get_filter!(Genre),
+						"連載情形" => status = get_filter!(Status),
+						"模式" => mode = get_filter!(Mode),
+						_ => continue,
+					}
+				}
+
+				FilterType::Sort => {
+					sort_by = filter
+						.value
+						.as_object()
+						.ok()
+						.and_then(|val| {
+							let i = val.get("index").as_int().ok()?.try_into().ok()?;
+
+							Sort::from_repr(i)
+						})
+						.unwrap_or_default();
+				}
+
+				FilterType::Genre => {
+					let is_not_checked = filter.value.as_int().unwrap_or(-1) != 1;
+					if is_not_checked {
+						continue;
+					}
+
+					tags.push(filter.name);
+				}
+
+				_ => continue,
+			}
+		}
+
+		Self::filters(genre, status, tags, mode, sort_by, page)
+	}
+}
+
+#[derive(Display, Default, FromRepr)]
+enum Genre {
+	#[default]
+	#[strum(to_string = "bl")]
+	Yaoi,
+
+	#[strum(to_string = "bg")]
+	Yuri,
+}
+
+#[derive(Display, Default, FromRepr)]
+enum Status {
+	#[default]
+	#[strum(to_string = "wj")]
+	Completed,
+
+	#[strum(to_string = "lz")]
+	Ongoing,
+}
+
+#[derive(IntoStaticStr, Clone, Copy, Default, FromRepr)]
+enum Mode {
+	#[default]
+	#[strum(to_string = "+")]
+	And,
+
+	#[strum(to_string = ",")]
+	Or,
+}
+
+#[derive(IntoStaticStr, Clone, Copy, Default, FromRepr)]
+enum Sort {
+	#[strum(to_string = "like")]
+	Likes,
+
+	#[default]
+	#[strum(to_string = "")]
+	LastUpdated,
+}
+
+struct FiltersQuery {
+	tags: Vec<String>,
+	mode: Mode,
+	sort_by: Sort,
+}
+
+impl FiltersQuery {
+	const fn new(tags: Vec<String>, mode: Mode, sort_by: Sort) -> Self {
+		Self {
+			tags,
+			mode,
+			sort_by,
+		}
+	}
+}
+
+impl Display for FiltersQuery {
+	fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+		let mut query = QueryParameters::new();
+
+		let tags = (!self.tags.is_empty()).then(|| {
+			self.tags
+				.iter()
+				.map(encode_uri_component)
+				.collect::<Vec<_>>()
+				.join(self.mode.into())
+		});
+		query.push_encoded("tag", tags.as_deref());
+
+		let sort_by = (!matches!(self.sort_by, Sort::LastUpdated)).then(|| self.sort_by.into());
+		query.push_encoded("sort", sort_by);
+
+		write!(f, "{query}")
+	}
+}
--- src/rust/zh.yandanshe/src/lib.rs
+++ src/rust/zh.yandanshe/src/lib.rs
@@ -3,14 +3,64 @@
 mod helper;
 
 use aidoku::{
-	prelude::{handle_notification, initialize},
-	std::String,
+	error::Result,
+	prelude::{get_manga_list, handle_notification, initialize},
+	std::{String, Vec},
+	Filter, Manga, MangaContentRating, MangaPageResult, MangaStatus,
 };
-use helper::setting::change_rate_limit;
+use helper::{setting::change_rate_limit, url::Url};
 
 #[initialize]
 fn initialize() {
 	change_rate_limit();
+}
+
+#[get_manga_list]
+fn get_manga_list(filters: Vec<Filter>, page: i32) -> Result<MangaPageResult> {
+	let filters_page = Url::from((filters, page)).html()?;
+	let manga = filters_page
+		.select("article.item")
+		.array()
+		.map(|val| {
+			let item = val.as_node()?;
+			let url = item.select("a").attr("href").read();
+
+			let id = url.replace(|c: char| !c.is_ascii_digit(), "");
+
+			let cover = item.select("img").attr("src").read();
+
+			let title = item.select("h3").text().read();
+
+			let categories = if item.select("span.label").text().read() == "會員" {
+				["會員專區".into()].into()
+			} else {
+				[].into()
+			};
+
+			let status = if item.select("footer").text().read().starts_with('全') {
+				MangaStatus::Completed
+			} else {
+				MangaStatus::Ongoing
+			};
+
+			let nsfw = MangaContentRating::Nsfw;
+
+			Ok(Manga {
+				id,
+				cover,
+				title,
+				url,
+				categories,
+				status,
+				nsfw,
+				..Default::default()
+			})
+		})
+		.collect::<Result<_>>()?;
+
+	let has_more = !filters_page.select("li.next-page a").array().is_empty();
+
+	Ok(MangaPageResult { manga, has_more })
 }
 
 #[expect(clippy::needless_pass_by_value)]
